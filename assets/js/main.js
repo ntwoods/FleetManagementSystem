68c34d94a9652e8b192e2b4c9821b62c6967a30d
ntwoods <mis01@ntwoods.com>
2025-07-01 11:48:00 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,21 @@
 
+// main.js
+document.addEventListener('DOMContentLoaded', async () => {
+  const table = document.getElementById('vehicleTable');
+  const vehicles = await fetchVehicles();
+
+  vehicles.forEach(vehicle => {
+    const row = table.insertRow();
+    row.innerHTML = `
+      <td>${vehicle.regNo}</td>
+      <td>${vehicle.type}</td>
+      <td>${vehicle.brand}</td>
+      <td>${vehicle.model}</td>
+      <td>${vehicle.owner}</td>
+      <td id="ins-${vehicle.id}">${vehicle.insuranceExpiry}</td>
+      <td id="puc-${vehicle.id}">${vehicle.pucExpiry}</td>
+    `;
+  });
+
+  setupReminders(vehicles);
+});
