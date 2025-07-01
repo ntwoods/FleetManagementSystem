7824ef92d6b9d9c1b13da97562473783a878441e
ntwoods <mis01@ntwoods.com>
2025-07-01 11:59:34 +0530

    Update fetchAPI.js

--- assets/js/fetchAPI.js
+++ assets/js/fetchAPI.js
@@ -1,6 +1,6 @@
 
 // fetchAPI.js
-const API_BASE = "https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec";
+const API_BASE = "https://script.google.com/macros/s/AKfycbxKVgQ3RMfkNsMOIKQGoEjl0fi4EhB1C08v4cJst1mONg65lDqP8ORehtJf9_qV1g3x/exec";
 
 async function fetchVehicles() {
   const res = await fetch(`${API_BASE}?action=getVehicles`);
